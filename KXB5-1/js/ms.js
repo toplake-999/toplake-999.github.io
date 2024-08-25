5ccf01c4057909737acc2b89715b581fc37a8bef
toplake-999 <tecstar@qq.com>
2024-08-25 14:01:29 +0800

    Add files via upload

---
+++ KXB5-1/js/ms.js
@@ -0,0 +1,52 @@
+
+    function updateClock() {
+      var now = new Date(); 
+      var hours = now.getHours();  
+      var minutes = now.getMinutes(); 
+      var seconds = now.getSeconds();  
+ 
+      
+      hours = checkTime(hours);
+      minutes = checkTime(minutes);
+      seconds = checkTime(seconds);
+      
+     
+      if(hours > 12 ) {
+          var timeString = hours + ":" + minutes + ":" + seconds;
+      } else {
+          var timeString = hours + ":" + minutes + ":" + seconds;
+      }
+      
+     
+      document.getElementById('xkxs').innerText = timeString;
+      
+     
+      setTimeout(updateClock, 1000);
+    }
+    
+    function checkTime(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
+    
+    
+    window.onload = function() {
+      updateClock();
+  var audio = new Audio('https://zjh-cms-transform.oss-cn-hangzhou.aliyuncs.com/ms%2F330000%2F20220926%2F82220f098809d49b1f88868c417240a6%2F231bc27c2b5fb65f25821437ff4c9f10.mp3');
+   audio.loop = true;
+ audio.play(); 
+    };
+    
+    
+    function getCurrentDate() {
+      var today = new Date();
+      var date = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+      var xkxs = today.getDay()
+      var weekdays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']; // 星期几名称的数组
+      var date1 = weekdays[xkxs]
+      document.getElementById('xkxs-date').textContent = date;
+      document.getElementById('xkxs-xq').textContent = date1;
+    }
+    getCurrentDate()
